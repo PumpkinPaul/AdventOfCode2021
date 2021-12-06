283e08eaff726890c6e6658f3e730eb98ca8b818
Paul Cunningham <paul.cunningham@pumpkin-games.net>
2021-12-06 17:30:48 +0000

    Added Part2

--- AdventOfCode/Day3.cs
+++ AdventOfCode/Day3.cs
@@ -6,7 +6,7 @@
 
         Console.WriteLine("RUNNING DAY #3");
         Console.WriteLine($"Part1: {Part1(lines)}");
-        //Console.WriteLine($"Part2: {Part2(lines)}");
+        Console.WriteLine($"Part2: {Part2(lines)}");
     }
 
     private static int Part1(string[] lines)
@@ -33,4 +33,35 @@
         var epsilon = maxValue - gamma; 
         return gamma * epsilon;
     }
-}
+
+    private static int Part2(string[] lines)
+    {
+        var oxygen = GetValue(lines, (zeros, ones) => ones >= zeros ? '1' : '0');
+        var c02 = GetValue(lines, (zeros, ones) => zeros <= ones ? '0' : '1');
+
+        return oxygen * c02;
+
+        static int GetValue(string[] lines, Func<int, int, char> predicate)
+        {
+            IEnumerable<string> matches = lines;
+
+            //Look at each bit (MSB to LSB)
+            for (var i = 0; i < lines[0].Length; i++)
+            {
+                matches = CheckBits(matches, i, predicate);
+                if (matches.Count() == 1)
+                    return Convert.ToInt32(matches.First(), 2);
+            }
+
+            return 0;
+
+            static IEnumerable<string> CheckBits(IEnumerable<string> lines, int bit, Func<int, int, char> predicate)
+            {
+                var zeros = lines.Count(line => line[bit] == '0');
+                var ones = lines.Count() - zeros;
+
+                return lines.Where(line => line[bit] == predicate(zeros, ones));
+            }
+        }
+    }
+ }
