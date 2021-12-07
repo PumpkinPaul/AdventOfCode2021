52d62eb9e392f33fc4418dcbe0e15dd1895ff96a
Paul Cunningham <paul.cunningham@pumpkin-games.net>
2021-12-07 08:32:00 +0000

    Day4 part2 - rather hacky

--- AdventOfCode/Day4.cs
+++ AdventOfCode/Day4.cs
@@ -9,16 +9,64 @@
         Console.WriteLine($"Part2: {Part2(lines)}");
     }
 
+    private static int Part1(string[] lines)
+    {
+        var (numbers, cards) = SetupGame(lines);
+
+        //Play bingo
+        foreach (var number in numbers)
+        {
+            foreach (var card in cards)
+            {
+                card.MarkNumber(number);
+
+                if (card.IsWinner)
+                    return number * card.UnmarkedSum;
+            }
+        }
+
+        return 0;
+    }
+
+    private static int Part2(string[] lines)
+    {
+        var (numbers, cards) = SetupGame(lines);
+
+        //Play bingo
+        var remainingCards = new List<Card>(cards);
+        var winningCards = new List<Card>();
+        foreach (var number in numbers)
+        {
+            foreach (var card in remainingCards)
+            {
+                card.MarkNumber(number);
+
+                if (card.IsWinner)
+                    winningCards.Add(card);
+            }
+
+            remainingCards = remainingCards.Except(winningCards).ToList();
+
+            if (remainingCards.Count == 0)
+                return number * winningCards.Last().UnmarkedSum;
+
+            winningCards.Clear();
+        }
+
+        return 0;
+    }
+
     class Card
     {
-        public const int CARD_ROWS = 5;
-        public const int CARD_COLS = 5;
+        public const int ROWS = 5;
+        public const int COLS = 5;
 
         private readonly int[][] _numbers; //[row][column]
+        private readonly HashSet<int> _markedNumbers = new();
 
         public Card(IList<string> lines)
         {
-            _numbers = new int[lines.Count()][];
+            _numbers = new int[lines.Count][];
 
             var row = 0;
             foreach (var line in lines)
@@ -33,27 +81,65 @@
 
         public void MarkNumber(int number)
         {
-
+            _markedNumbers.Add(number);
         }
 
         public bool IsWinner
         {
             get
             {
+                for (var row = 0; row < ROWS; row++)
+                {
+                    var markedCount = 0;
+                    for (var col = 0; col < COLS; col++)
+                    {
+                        if (_markedNumbers.Contains(_numbers[row][col]))
+                            markedCount++;
+                    }
+
+                    if (markedCount == COLS)
+                        return true;
+                }
+
+                for (var col = 0; col < COLS; col++)
+                {
+                    var markedCount = 0;
+                    for (var row = 0; row < ROWS; row++)
+                    {
+                        if (_markedNumbers.Contains(_numbers[row][col]))
+                            markedCount++;
+                    }
+
+                    if (markedCount == ROWS)
+                        return true;
+                }
+
+
                 return false;
             }
         }
 
-        public int SumUnmarked
+        public int UnmarkedSum
         {
             get
             {
-                return 0;
+                var total = 0;
+                for (var row = 0; row < ROWS; row++)
+                {
+                    for (var col = 0; col < COLS; col++)
+                    {
+                        var number = _numbers[row][col];
+                        if (_markedNumbers.Contains(number) == false)
+                            total += number;
+                    }
+                }
+
+                return total;
             }
         }
     }
 
-    private static int Part1(string[] lines)
+    private static (IEnumerable<int> numbers, IEnumerable<Card> cards) SetupGame(string[] lines)
     {
         var numbers = lines[0].Split(',').Select(number => int.Parse(number));
         var cards = new List<Card>();
@@ -65,31 +151,13 @@
                 continue;
 
             cardLines.Add(line);
-            if (cardLines.Count == Card.CARD_ROWS)
+            if (cardLines.Count == Card.ROWS)
             {
                 cards.Add(new Card(cardLines));
                 cardLines.Clear();
-            }            
-        }
-
-        foreach(var number in numbers)
-        {
-            foreach (var card in cards)
-            {
-                card.MarkNumber(number);
-
-                if (card.IsWinner)
-                {
-                    return number * card.SumUnmarked;
-                }
             }
         }
 
-        return 0;
-    }
-
-    private static int Part2(string[] lines)
-    {
-        return 0;
+        return (numbers, cards);
     }
 }
