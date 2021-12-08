c006e71d9b5c86e503b0470136e5d47178440f15
Paul Cunningham <paul.cunningham@pumpkin-games.net>
2021-12-08 14:53:05 +0000

    Day8 part1

---
+++ AdventOfCode/Day8.cs
@@ -0,0 +1,24 @@
+﻿public class Day8
+{
+    public static void Run()
+    {
+        var lines = File.ReadAllLines("Input8.txt");
+
+        Console.WriteLine("--- Day 8: Seven Segment Search ---");
+
+        Console.WriteLine($"Test1: {Solve(lines)}");
+    }
+
+    private static long Solve(string[] lines)
+    {
+        //A count of the number of segments for the numbers we are interested in counting (1, 4, 7, 8)
+        var sevenSegmentDigits = new HashSet<int>{{ 2 }, { 4 }, { 3 }, { 7 }};
+
+        return lines
+            .SelectMany(ParseOutputValues)
+            .Count(value => sevenSegmentDigits.Contains(value.Length));
+
+        static IEnumerable<string>ParseOutputValues(string line) => 
+            line.Split('|')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -4,4 +4,5 @@
 //Day4.Run();
 //Day5.Run();
 //Day6.Run();
-Day7.Run();
+//Day7.Run();
+Day8.Run();
