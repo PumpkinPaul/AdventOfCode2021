8a960329d666705359c4aadce833c775b4be51c9
Paul Cunningham <paul.cunningham@pumpkin-games.net>
2021-12-12 17:54:13 +0000

    Day12 part2 done!

--- AdventOfCode/Day12.cs
+++ AdventOfCode/Day12.cs
@@ -7,18 +7,18 @@
         var test1 = File.ReadAllLines("Test12_1.txt");
         var test2 = File.ReadAllLines("Test12_2.txt");
         var test3 = File.ReadAllLines("Test12_3.txt");
-        Console.WriteLine($"Test1: {Part1(test1)}"); //10
-        Console.WriteLine($"Test1: {Part1(test2)}"); //19
-        Console.WriteLine($"Test1: {Part1(test3)}"); //226
-        //Console.WriteLine($"Test2: {Part2(test)}"); //
+        Console.WriteLine($"Test1(1): {Part1(test1)}"); //10
+        Console.WriteLine($"Test1(2): {Part1(test2)}"); //19
+        Console.WriteLine($"Test1(3): {Part1(test3)}"); //226
+        
+        Console.WriteLine($"Test2(1): {Part2(test1)}"); //36
+        Console.WriteLine($"Test2(2): {Part2(test2)}"); //103
+        Console.WriteLine($"Test2(3): {Part2(test3)}"); //3509
 
         var lines = File.ReadAllLines("Input12.txt");
         Console.WriteLine($"Part1: {Part1(lines)}"); //4104
-        //Console.WriteLine($"Part2: {Part2(lines)}"); //
+        Console.WriteLine($"Part2: {Part2(lines)}"); //119760
     }
-
-
-    private enum RouteStatuses { FOUND_VALID_ROUTE, CONTINUE }
 
     private static long Part1(string[] lines)
     {
@@ -40,15 +40,15 @@
             {
                 //Reset all caves as not visted...
                 foreach (var k in caves)
-                    k.Value.Visited = false;
+                    k.Value.Visited = 0;
 
                 //...set visited caves
                 foreach (var caveKey in route.Split(','))
-                    caves[caveKey].Visited = true;
+                    caves[caveKey].Visited++;
             }
 
             //Can only visit small caves once
-            if (cave.CaveType != CaveTypes.LARGE && cave.Visited)
+            if (cave.CaveType != CaveTypes.LARGE && cave.Visited == 1)
                 return;
 
             //This is a cave we can enter so append it to the current route
@@ -66,19 +66,76 @@
                 Navigate(connectedCave.Key, route);
         }
 
-        //foreach (var route in routes)
-        //    Console.WriteLine(route);
-
         return routes.Count;
     }
 
     private static long Part2(string[] lines)
     {
         //====================================================================================================
-        //
+        //Given these new rules, how many paths through this cave system are there?
         //====================================================================================================
 
-        return 0;
+        //====================================================================================================
+        //How many paths through this cave system are there that visit small caves at most once?
+        //====================================================================================================
+
+        var routes = new List<string>();
+        var caves = LoadCaveSystem(lines);
+
+        Navigate("start", "", "");
+
+        void Navigate(string name, string route, string smallCaveVisitedTwice)
+        {
+            var cave = caves[name];
+
+            //Apply cave state from route
+            if (string.IsNullOrWhiteSpace(route) == false)
+            {
+                //Reset all caves as not visted...
+                foreach (var k in caves)
+                    k.Value.Visited = 0;
+
+                //...set visited caves
+                foreach (var caveKey in route.Split(','))
+                    caves[caveKey].Visited++;
+            }
+
+            if (cave.CaveType == CaveTypes.START && cave.Visited == 1)
+                return;
+
+            if (cave.CaveType == CaveTypes.END && cave.Visited == 1)
+                return;
+
+            if (cave.CaveType == CaveTypes.SMALL)
+            {
+                if (cave.Visited == 1)
+                {
+                    if (string.IsNullOrWhiteSpace(smallCaveVisitedTwice))
+                        smallCaveVisitedTwice = cave.Name;
+
+                    if (smallCaveVisitedTwice != cave.Name)
+                        return;
+                }
+                else if (cave.Visited == 2)
+                    return;
+            }
+
+            //This is a cave we can enter so append it to the current route
+            route += (string.IsNullOrWhiteSpace(route) ? "" : ",") + name;
+
+            //Flag the route as valid if we've reached the end cave
+            if (cave.CaveType == CaveTypes.END)
+            {
+                routes.Add(route);
+                return;
+            }
+
+            //Continue into caves that are connected to this cave...
+            foreach (var connectedCave in cave.Connected)
+                Navigate(connectedCave.Key, route, smallCaveVisitedTwice);
+        }
+
+        return routes.Count;
     }
 
     //Load the puzzle input energy values into a 2d array [rows][cols]
@@ -116,7 +173,7 @@
     {
         public readonly string Name;
         public readonly CaveTypes CaveType;
-        public bool Visited;
+        public int Visited;
         public readonly Dictionary<string, Cave> Connected = new();
 
         public Cave(string name)
